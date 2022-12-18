79218b7933227b187a93174d0767eb0d5ca94426
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2022-12-18 05:45:30 +0100

    Replace generator files with LibraryGenerator

--- LibraryGenerator/GameNetworkingSocketsLibrary.cs
+++ LibraryGenerator/GameNetworkingSocketsLibrary.cs
@@ -1,68 +1,159 @@
 using CppSharp;
 using CppSharp.AST;
 using CppSharp.Generators;
+using System.Collections.Generic;
 using System;
 using System.IO;
+using CppSharp.Parser;
 
 namespace LibraryGenerator;
 
 public class GameNetworkingSocketsLibrary : ILibrary
 {
-    private readonly string _repoPath;
-    private readonly string _buildPath;
+    private const string DotNetLibraryName = "Valve.Sockets";
+    private const string LibraryName = "GameNetworkingSockets";
+
     private readonly string _outputPath;
+
+    private readonly string _includePath;
+    private readonly string _steamIncludePath;
+    private readonly string _sourcePath;
+    private readonly string _libraryPath;
+    private readonly List<string> _headerFiles;
+    private readonly List<string> _sourceFiles;
+    private readonly List<string> _includeDirs;
+
+    private void LogFiles()
+    {
+        Console.WriteLine("Header files:");
+        foreach (var file in _headerFiles)
+        {
+            Console.WriteLine($"\t{file}");
+        }
+
+        Console.WriteLine("Source files:");
+        foreach (var file in _sourceFiles)
+        {
+            Console.WriteLine($"\t{file}");
+        }
+    }
 
     public GameNetworkingSocketsLibrary(string repoPath, string buildPath, string outputPath)
     {
-        _repoPath = repoPath;
-        _buildPath = buildPath;
         _outputPath = outputPath;
+
+        _includePath = Path.Combine(repoPath, "include");
+        _steamIncludePath = Path.Combine(_includePath, "steam");
+        _sourcePath = Path.Combine(repoPath, "src");
+        _libraryPath = Path.Combine(buildPath, "bin");
+
+        _includeDirs = new List<string>
+        {
+            _includePath,
+            // _steamIncludePath
+            // Path.Combine(_sourcePath, "public"),
+            // Path.Combine(_sourcePath, "common"),
+            // Path.Combine(buildPath, "src")
+        };
+
+        _headerFiles = new List<string>
+        {
+            Path.Combine(_steamIncludePath, "steamnetworkingsockets.h"),
+            Path.Combine(_steamIncludePath, "steamnetworkingsockets_flat.h"),
+            // Path.Combine(_steamIncludePath, "isteamnetworkingmessages.h"),
+            // Path.Combine(_steamIncludePath, "isteamnetworkingutils.h"),
+            // Path.Combine(_steamIncludePath, "steamnetworkingcustomsignaling.h"),
+            // Path.Combine(_steamIncludePath, "steamnetworkingtypes.h"),
+            // Path.Combine(_steamIncludePath, "steamtypes.h"),
+            // Path.Combine(_steamIncludePath, "steamuniverse.h"),
+            // Path.Combine(_steamIncludePath, "steamclientpublic.h")
+        };
+
+        _sourceFiles = new List<string>();
+        // _sourceFiles.AddRange(
+        //     Directory.GetFiles(Path.Combine(_sourcePath, "steamnetworkingsockets"), "*.cpp")
+        // );
+        // _sourceFiles.AddRange(
+        //     Directory.GetFiles(Path.Combine(_sourcePath, "steamnetworkingsockets", "clientlib"), "*.cpp")
+        // );
+
+        // _headerFiles.AddRange(
+        //     Directory.GetFiles(Path.Combine(_sourcePath, "steamnetworkingsockets"), "*.h")
+        // );
+        // _headerFiles.AddRange(
+        //     Directory.GetFiles(Path.Combine(_sourcePath, "steamnetworkingsockets", "clientlib"), "*.h")
+        // );
+
+        LogFiles();
     }
 
     public void Setup(Driver driver)
     {
-        if (!OperatingSystem.IsWindows())
-        {
-            driver.ParserOptions.MicrosoftMode = false;
-            driver.ParserOptions.TargetTriple = "x86_64-linux-gnu";
-        }
-        else
-        {
-            driver.ParserOptions.MicrosoftMode = true;
-        }
+        driver.ParserOptions.LanguageVersion = LanguageVersion.CPP11_GNU;
+        driver.ParserOptions.SkipPrivateDeclarations = true;
+        driver.ParserOptions.MicrosoftMode = false;
+        driver.ParserOptions.TargetTriple = "x86_64-linux-gnu";
 
         driver.Options.GeneratorKind = GeneratorKind.CSharp;
-        driver.Options.UseHeaderDirectories = true;
         driver.Options.GenerationOutputMode = GenerationOutputMode.FilePerUnit;
-        driver.Options.CommentKind = CommentKind.BCPLSlash;
+        driver.Options.GenerateDeprecatedDeclarations = true;
+        driver.Options.StripLibPrefix = true;
+        driver.Options.CheckSymbols = true;
+        driver.Options.UseSpan = true;
+        driver.Options.GenerateDefaultValuesForArguments = true;
+        driver.Options.MarshalCharAsManagedChar = true;
+
         driver.Options.OutputDir = _outputPath;
+
+        // driver.Options.GenerateDebugOutput = true;
         driver.Options.Verbose = true;
 
-        var module = driver.Options.AddModule("Valve.Sockets");
+        var module = driver.Options.AddModule(LibraryName);
+        module.OutputNamespace = DotNetLibraryName;
 
-        module.IncludeDirs.Add(Path.Combine(_repoPath, "include", "steam"));
+        module.IncludeDirs.AddRange(_includeDirs);
+        module.Headers.AddRange(_headerFiles);
 
-        module.Headers.Add(Path.Combine(_repoPath, "include", "steam", "isteamnetworkingsockets.h"));
-        module.Headers.Add(Path.Combine(_repoPath, "include", "steam", "steamnetworkingtypes.h"));
+        module.LibraryDirs.Add(_libraryPath);
+        module.Libraries.Add($"lib{LibraryName}.so");
 
-        module.LibraryDirs.Add(Path.Combine(_buildPath, "bin"));
-        module.Libraries.Add("libGameNetworkingSockets.so");
-
-        module.OutputNamespace = "Valve.Sockets";
+        module.CodeFiles.AddRange(_sourceFiles);
     }
 
     public void SetupPasses(Driver driver)
     {
-        Console.WriteLine("SetupPasses called.");
+
     }
 
     public void Preprocess(Driver driver, ASTContext ctx)
     {
-        Console.WriteLine("Preprocess called.");
+        // ctx.IgnoreClassWithName("ISteamNetworkingFakeUDPPort");
     }
 
     public void Postprocess(Driver driver, ASTContext ctx)
     {
-        Console.WriteLine("Postprocess called.");
+        foreach (var unit in ctx.TranslationUnits)
+        {
+            foreach (var unitClass in unit.Classes)
+            {
+                if (unitClass.Name.StartsWith("I"))
+                {
+                    unitClass.Type = ClassType.Interface;
+                    unitClass.IsAbstract = false;
+                }
+            }
+        }
+    }
+
+    public void GenerateCode(Driver driver, List<GeneratorOutput> outputs)
+    {
+        foreach (var output in outputs)
+        {
+            if (output.TranslationUnit.FileName == "Std.h")
+            {
+                output.Outputs.Clear();
+                break;
+            }
+        }
     }
 }
--- LibraryGenerator/Program.cs
+++ LibraryGenerator/Program.cs
@@ -1,95 +1,223 @@
-﻿using System.Diagnostics;
-using System.Reflection;
-using CppSharp;
-using System;
+﻿using System;
 using System.IO;
-using System.Threading;
+using System.Linq;
+using System.Text;
 
 namespace LibraryGenerator;
 
 public static class Program
 {
-    private const string Repo = "GameNetworkingSockets";
-    private const string RepoUrl = $"https://github.com/ValveSoftware/{Repo}";
-    private const string BuildingUrl = $"{RepoUrl}/blob/master/BUILDING.md";
+    private const string PInvokeGenerator = "ClangSharpPInvokeGenerator";
+    private const string LlvmRepo = "llvm-project";
+    private const string LlvmRepoUrl = $"https://github.com/llvm/{LlvmRepo}";
+    private const string LlvmBranch = "llvmorg-15.0.0";
+    private const string ClangSharpRepo = "clangsharp";
+    private const string ClangSharpRepoUrl = $"https://github.com/dotnet/{ClangSharpRepo}";
+    private const string GNSRepo = "GameNetworkingSockets";
+    private const string GNSRepoUrl = $"https://github.com/ValveSoftware/{GNSRepo}";
+
     private const string OutputName = "Valve.Sockets";
+
+    private static string GetPInvokeGeneratorArgs(string gnsDir, string outputDir)
+    {
+        string gccVersionPath = Directory.GetDirectories("/usr/lib/gcc/x86_64-pc-linux-gnu").Max();
+        string gnsIncludeSteamDir = Path.Combine(gnsDir, "include", "steam");
+        StringBuilder builder = new();
+
+        // Configuration options
+        builder.AppendLine("--config");
+        builder.AppendLine("log-exclusions");
+        builder.AppendLine("log-potential-typedef-remappings");
+        builder.AppendLine("multi-file");
+        builder.AppendLine("compatible-codegen");
+        builder.AppendLine("unix-types");
+
+        // Include directories
+        builder.AppendLine("--include-directory");
+        builder.AppendLine(Path.Combine(gnsDir, "include"));
+        builder.AppendLine(Path.Combine(gccVersionPath, "include"));
+
+        // Header files
+        builder.AppendLine("--file");
+        builder.AppendLine(Path.Combine(gnsIncludeSteamDir, "isteamnetworkingutils.h"));
+        builder.AppendLine(Path.Combine(gnsIncludeSteamDir, "isteamnetworkingsockets.h"));
+        builder.AppendLine(Path.Combine(gnsIncludeSteamDir, "isteamnetworkingmessages.h"));
+        builder.AppendLine(Path.Combine(gnsIncludeSteamDir, "steamnetworkingsockets.h"));
+        builder.AppendLine(Path.Combine(gnsIncludeSteamDir, "steamnetworkingsockets_flat.h"));
+        builder.AppendLine(Path.Combine(gnsIncludeSteamDir, "steamnetworkingtypes.h"));
+        builder.AppendLine(Path.Combine(gnsIncludeSteamDir, "steamtypes.h"));
+        builder.AppendLine(Path.Combine(gnsIncludeSteamDir, "steamuniverse.h"));
+
+        // Generated namespace
+        builder.AppendLine("--namespace");
+        builder.AppendLine(OutputName);
+
+        // Generated PInvoke filename
+        builder.AppendLine("--methodClassName");
+        builder.AppendLine("Native");
+
+        // Native library name
+        builder.AppendLine("--libraryPath");
+        builder.AppendLine(GNSRepo);
+
+        // Output path
+        builder.AppendLine("--output");
+        builder.AppendLine(outputDir);
+
+        return builder.Replace("\n", " ").ToString();
+    }
+
     public static int Main(string[] args)
     {
-        string outputDir = Path.Join(Path.GetFullPath(args.Length > 0 ? args[0] : Directory.GetCurrentDirectory()), OutputName);
-        string programDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
-        string repoDir = Path.Join(programDir, Repo);
-        string buildDir = Path.Join(repoDir, "build");
+        string workDir = Path.Combine(Path.GetFullPath(args.Length > 0 ? args[0] : Directory.GetCurrentDirectory()), "gen");
+        string outputDir = Path.Combine(Path.GetFullPath(args.Length > 1 ? args[1] : Directory.GetCurrentDirectory()), OutputName);
 
-        Console.WriteLine("Make sure git and the required packages are installed.");
-        Console.WriteLine($"You can read the build guide here: {BuildingUrl}");
+        Console.WriteLine("Options:");
+        Console.WriteLine($"\tWork directory: {workDir}");
+        Console.WriteLine($"\tOutput directory: {outputDir}");
 
-        Console.WriteLine("\nOptions:");
-        Console.WriteLine($"\tRepository path: {repoDir}");
-        Console.WriteLine($"\tBuild path: {buildDir}");
-        Console.WriteLine($"\tOutput path: {outputDir}");
-
-        Thread.Sleep(2000);
-        Console.WriteLine("\nPreparing...");
-
-        bool repoExists = Directory.Exists(repoDir);
-
-        if (repoExists && Directory.Exists(buildDir))
+        if (!Directory.Exists(workDir))
         {
-            Console.WriteLine("Existing repository found!");
-            Directory.Delete(buildDir, true);
+            Directory.CreateDirectory(workDir);
         }
 
-        if (!repoExists)
+        // Phase 1: Install dotnet tool
+
+        ProcessHelper.InstallGlobalDotnetTool(PInvokeGenerator);
+
+        // Phase 2: Clone repos
+
+        Console.WriteLine("\nCloning repos...");
+
+        if (!ProcessHelper.CloneGitRepo(LlvmRepoUrl, workDir, extraOptions: $"--single-branch --branch {LlvmBranch}"))
         {
-            Console.WriteLine($"Cloning {Repo}...");
+            Console.WriteLine($"Failed to clone {LlvmRepo}. Aborting.");
 
-            using Process gitProcess = new();
-            gitProcess.StartInfo.FileName = "git";
-            gitProcess.StartInfo.Arguments = $"clone {RepoUrl}";
-            gitProcess.StartInfo.WorkingDirectory = programDir;
-            gitProcess.Start();
-            gitProcess.WaitForExit();
+            return 1;
+        }
 
-            if (gitProcess.ExitCode != 0)
+        if (!ProcessHelper.CloneGitRepo(ClangSharpRepoUrl, workDir))
+        {
+            Console.WriteLine($"Failed to clone {ClangSharpRepo}. Aborting.");
+
+            return 1;
+        }
+
+        if (!ProcessHelper.CloneGitRepo(GNSRepoUrl, workDir))
+        {
+            Console.WriteLine($"Failed to clone {GNSRepo}. Aborting.");
+
+            return 1;
+        }
+
+        // Phase 3: Build dependencies for libClangSharp
+
+        Console.WriteLine("\nBuilding dependencies...");
+
+        string clangBuildPath = Path.Combine(workDir, LlvmRepo, "artifacts", "bin");
+        string clangInstallPath = Path.Combine(workDir, LlvmRepo, "artifacts", "install");
+
+        bool cmakeResult = ProcessHelper.InvokeCmake(
+            Path.Combine(workDir, LlvmRepo, "llvm"),
+            clangBuildPath,
+            "-DLLVM_ENABLE_PROJECTS=clang -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_BUILD_TYPE=Release -G \"Unix Makefiles\""
+        );
+
+        if (!cmakeResult)
+        {
+            Console.WriteLine($"Couldn't generate CMake project for {LlvmRepo}/{LlvmBranch}. Aborting.");
+
+            return 1;
+        }
+
+        if (!File.Exists(Path.Combine(clangInstallPath, "lib", "libclang.so")))
+        {
+            Console.WriteLine("Building clang...");
+
+            if (!ProcessHelper.BuildMakeProject(clangBuildPath))
             {
-                Console.WriteLine($"\nCouldn't clone repository. (Exit code: {gitProcess.ExitCode})");
+                Console.WriteLine("Failed to build clang. Aborting.");
+
                 return 1;
             }
+
+            Console.WriteLine("Installing clang...");
+
+            if (!ProcessHelper.BuildMakeProject(clangBuildPath, "install"))
+            {
+                Console.WriteLine("Failed to install clang. Aborting.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Clang already built and installed.");
         }
 
-        Console.WriteLine("Creating build directory...");
-        Directory.CreateDirectory(buildDir);
+        // Phase 4: Build libClangSharp
 
-        Console.WriteLine("Generating project...");
+        string clangSharpBuildPath = Path.Combine(workDir, ClangSharpRepo, "artifacts", "bin", "native");
 
-        using Process cmakeProcess = new();
-        cmakeProcess.StartInfo.FileName = "cmake";
-        cmakeProcess.StartInfo.Arguments = "-G Ninja ..";
-        cmakeProcess.StartInfo.WorkingDirectory = buildDir;
-        cmakeProcess.Start();
-        cmakeProcess.WaitForExit();
+        cmakeResult = ProcessHelper.InvokeCmake(
+            Path.Combine(workDir, ClangSharpRepo),
+            clangSharpBuildPath,
+            $"-DPATH_TO_LLVM=\"{clangInstallPath}\""
+        );
 
-        Console.WriteLine("\nBuilding project...");
+        if (!cmakeResult)
+        {
+            Console.WriteLine($"Couldn't generate CMake project for {ClangSharpRepo}. Aborting.");
 
-        using Process ninjaProcess = new();
-        ninjaProcess.StartInfo.FileName = "ninja";
-        ninjaProcess.StartInfo.WorkingDirectory = buildDir;
-        ninjaProcess.Start();
-        ninjaProcess.WaitForExit();
+            return 1;
+        }
 
-        Console.WriteLine("\nDone!");
+        if (!ProcessHelper.BuildMakeProject(clangSharpBuildPath))
+        {
+            Console.WriteLine("Failed to build libClangSharp. Aborting.");
+
+            return 1;
+        }
+
+        // Phase 5: Invoke PInvokeGenerator
+
+        Console.WriteLine($"\nPreparing {PInvokeGenerator}...");
+
+        string clangSharpLibPath = Path.Combine(clangSharpBuildPath, "lib");
+        string clangLibPath = Path.Combine(clangInstallPath, "lib");
 
         if (Directory.Exists(outputDir))
         {
-            Console.WriteLine($"Cleaning existing output path: {outputDir}");
+            Console.WriteLine($"Cleaning existing output directory: {outputDir}");
             Directory.Delete(outputDir, true);
         }
 
         Directory.CreateDirectory(outputDir);
 
-        Console.WriteLine("\nGenerating bindings...");
+        Console.WriteLine($"Generating {GNSRepo} bindings...");
 
-        ConsoleDriver.Run(new GameNetworkingSocketsLibrary(repoDir, buildDir, outputDir));
+        int generatorResult = ProcessHelper.InvokeWithEnvVars(
+            PInvokeGenerator,
+            GetPInvokeGeneratorArgs(Path.Combine(workDir, GNSRepo), outputDir),
+            workDir, new()
+            {
+                {"LD_LIBRARY_PATH", $"{clangSharpLibPath}:{clangLibPath}"}
+            }
+        );
+
+        if (generatorResult != 0 && generatorResult != 2)
+        {
+            Console.WriteLine($"{PInvokeGenerator} couldn't generate {GNSRepo} bindings. Aborting.");
+
+            return 1;
+        }
+
+        Console.WriteLine("Success!");
+
+        // Phase 6: Run CppSharp source generation passes
+
+        // Console.WriteLine("\nGenerating bindings...");
+
+        // ConsoleLibraryDriver.Run(new GameNetworkingSocketsLibrary(repoDir, buildDir, outputDir));
+        // ConsoleDriver.Run(new GameNetworkingSocketsLibrary(repoDir, buildDir, outputDir));
 
         return 0;
     }
