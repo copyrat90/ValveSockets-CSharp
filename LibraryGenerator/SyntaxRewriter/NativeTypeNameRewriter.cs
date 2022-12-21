4b69fdb276a81d83ec533cab6843c57d77ac8d8c
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2022-12-22 00:55:41 +0100

    Add handling for predefined types to NativeTypeNameRewriter

--- LibraryGenerator/SyntaxRewriter/NativeTypeNameRewriter.cs
+++ LibraryGenerator/SyntaxRewriter/NativeTypeNameRewriter.cs
@@ -1,66 +1,190 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using System;
+using System.Linq;
 
 namespace LibraryGenerator.SyntaxRewriter;
 
 public class NativeTypeNameRewriter : CSharpSyntaxRewriter, ISyntaxRewriter
 {
-    private SyntaxNode RewriteMethodReturnType(MethodDeclarationSyntax node)
+    private static TypeSyntax ExtractNativeType(AttributeArgumentSyntax argument)
     {
-        return node;
-    }
+        var nativeType = argument.Expression.ToString().Trim('"');
 
-    private SyntaxNode RewriteParameterType(ParameterSyntax node)
-    {
-        return node;
-    }
-
-    private SyntaxNode RewriteFieldType(FieldDeclarationSyntax node)
-    {
-        return node;
-    }
-
-    private SyntaxNode RewriteEnumType(EnumDeclarationSyntax node)
-    {
-        return node;
-    }
-
-    private SyntaxNode RewritePropertyType(PropertyDeclarationSyntax node)
-    {
-        return node;
-    }
-
-    public override SyntaxNode VisitAttribute(AttributeSyntax node)
-    {
-        if (node.Name.ToString() == "NativeTypeName")
+        switch (nativeType)
         {
-            var parent = (node.Parent as AttributeListSyntax)!;
-            var targetNode = parent.Parent!;
+            case "uint64":
+                nativeType = "ulong";
+                break;
+            case "uint32":
+                nativeType = "uint";
+                break;
+            case "uint16":
+                nativeType = "ushort";
+                break;
+            case "int64":
+                nativeType = "long";
+                break;
+        }
 
-            if (parent.Target?.Identifier.ToString() == "return")
+        return SyntaxFactory.ParseTypeName(nativeType);
+    }
+
+    private static bool TryGetNativeTypeName(SyntaxList<AttributeListSyntax> attributeLists, out TypeSyntax nativeType)
+    {
+        nativeType = null;
+
+        foreach (AttributeListSyntax attributeList in attributeLists)
+        {
+            AttributeSyntax nativeTypeAttribute = attributeList.Attributes.FirstOrDefault(attribute => attribute.Name.ToString() == "NativeTypeName", null);
+
+            if (nativeTypeAttribute != null)
             {
-                return node.ReplaceNode(targetNode, RewriteMethodReturnType(targetNode as MethodDeclarationSyntax));
+                nativeType = ExtractNativeType(nativeTypeAttribute.ArgumentList!.Arguments.First());
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static T GetNativeTypeNode<T>(T currentType, TypeSyntax nativeType) where T : SyntaxNode
+    {
+        if (currentType.IsKind(SyntaxKind.PredefinedType) && nativeType.IsKind(SyntaxKind.PredefinedType))
+        {
+            if (currentType.ToString() != nativeType.ToString())
+            {
+                return nativeType.WithTriviaFrom(currentType) as T;
             }
 
-            switch (targetNode.Kind())
+            return currentType;
+        }
+
+        // TODO: Add support for IdentifierName
+        // TODO: Add support for PointerType
+
+        if (currentType.IsKind(SyntaxKind.Parameter) && currentType is ParameterSyntax currentParameter)
+        {
+            return currentParameter.WithType(GetNativeTypeNode(currentParameter.Type, nativeType)) as T;
+        }
+
+        if (currentType.IsKind(SyntaxKind.OperatorDeclaration) && currentType is OperatorDeclarationSyntax currentOperator)
+        {
+            return currentOperator.WithReturnType(GetNativeTypeNode(currentOperator.ReturnType, nativeType)) as T;
+        }
+
+        if (currentType.IsKind(SyntaxKind.FieldDeclaration) && currentType is FieldDeclarationSyntax currentField)
+        {
+            return currentField.WithDeclaration(
+                currentField.Declaration.WithType(GetNativeTypeNode(currentField.Declaration.Type, nativeType))) as T;
+        }
+
+        if (currentType.IsKind(SyntaxKind.PropertyDeclaration) && currentType is PropertyDeclarationSyntax currentProperty)
+        {
+            return currentProperty.WithType(GetNativeTypeNode(currentProperty.Type, nativeType)) as T;
+        }
+
+        return currentType;
+    }
+
+    public override SyntaxNode VisitEnumDeclaration(EnumDeclarationSyntax node)
+    {
+        if (!TryGetNativeTypeName(node.AttributeLists, out TypeSyntax nativeType))
+        {
+            return node;
+        }
+
+        // node.BaseList.Types is a nightmare.
+
+        // Console.WriteLine($"\tEnums are not supported. {node.Identifier} currentType: {node.BaseList?.Types.FirstOrDefault()} nativeType: {nativeType}");
+
+        return node;
+    }
+
+    public override SyntaxNode VisitFieldDeclaration(FieldDeclarationSyntax node)
+    {
+        if (!TryGetNativeTypeName(node.AttributeLists, out TypeSyntax nativeType))
+        {
+            return node;
+        }
+
+        return node.ReplaceNode(node, GetNativeTypeNode(node, nativeType));
+    }
+
+    public override SyntaxNode VisitMethodDeclaration(MethodDeclarationSyntax node)
+    {
+        if (TryGetNativeTypeName(node.AttributeLists, out TypeSyntax nativeType))
+        {
+            node = node.ReplaceNode(node, node.WithReturnType(GetNativeTypeNode(node.ReturnType, nativeType)));
+        }
+
+        foreach (var childNode in node.ChildNodes())
+        {
+            if (childNode.IsKind(SyntaxKind.ParameterList) && childNode is ParameterListSyntax parameterList)
             {
-                case SyntaxKind.Parameter:
-                    return node.ReplaceNode(targetNode, RewriteParameterType(targetNode as ParameterSyntax));
-                case SyntaxKind.FieldDeclaration:
-                    return node.ReplaceNode(targetNode, RewriteFieldType(targetNode as FieldDeclarationSyntax));
-                case SyntaxKind.EnumDeclaration:
-                    return node.ReplaceNode(targetNode, RewriteEnumType(targetNode as EnumDeclarationSyntax));
-                case SyntaxKind.PropertyDeclaration:
-                    return node.ReplaceNode(targetNode, RewritePropertyType(targetNode as PropertyDeclarationSyntax));
-                default:
-                    Console.WriteLine($"\tNo conversion found for target node kind: {targetNode.Kind()}");
-                    break;
+                bool parameterListChanged = false;
+                var newParameterList = SyntaxFactory.SeparatedList<ParameterSyntax>();
+
+                foreach (ParameterSyntax parameter in parameterList.Parameters)
+                {
+                    if (!TryGetNativeTypeName(parameter.AttributeLists, out TypeSyntax paramNativeType))
+                    {
+                        newParameterList = newParameterList.Add(parameter);
+                        continue;
+                    }
+
+                    var newParameter = GetNativeTypeNode(parameter, paramNativeType);
+
+                    if (newParameter != parameter)
+                    {
+                        newParameterList = newParameterList.Add(newParameter);
+                        parameterListChanged = true;
+                    }
+                    else
+                    {
+                        newParameterList = newParameterList.Add(parameter);
+                    }
+                }
+
+                if (parameterListChanged)
+                {
+                    node = node.ReplaceNode(parameterList, parameterList.WithParameters(newParameterList));
+                }
             }
         }
 
         return node;
+    }
+
+    public override SyntaxNode VisitOperatorDeclaration(OperatorDeclarationSyntax node)
+    {
+        if (!TryGetNativeTypeName(node.AttributeLists, out TypeSyntax nativeType))
+        {
+            return node;
+        }
+
+        return node.ReplaceNode(node, GetNativeTypeNode(node, nativeType));
+    }
+
+    public override SyntaxNode VisitPropertyDeclaration(PropertyDeclarationSyntax node)
+    {
+        if (!TryGetNativeTypeName(node.AttributeLists, out TypeSyntax nativeType))
+        {
+            return node;
+        }
+
+        return node.ReplaceNode(node, GetNativeTypeNode(node, nativeType));
+    }
+
+    public override SyntaxNode VisitParameter(ParameterSyntax node)
+    {
+        if (!TryGetNativeTypeName(node.AttributeLists, out TypeSyntax nativeType))
+        {
+            return node;
+        }
+
+        return node.ReplaceNode(node, GetNativeTypeNode(node, nativeType));
     }
 
     public SyntaxNode FixupVisit(SyntaxNode node)
