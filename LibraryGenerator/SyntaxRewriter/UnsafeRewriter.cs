10805ef96fb5822231dcc4b54c789fa2b5ddb3fa
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2022-12-22 03:22:18 +0100

    Add FixReturnStatements() for ref return types
    This didn't work until now and it took me hours to debug this.
    Seems like you can only use ReplaceNode once per Visit() call.

--- LibraryGenerator/SyntaxRewriter/UnsafeRewriter.cs
+++ LibraryGenerator/SyntaxRewriter/UnsafeRewriter.cs
@@ -6,12 +6,12 @@ namespace LibraryGenerator.SyntaxRewriter;
 
 public class UnsafeRewriter : CSharpSyntaxRewriter, ISyntaxRewriter
 {
+    private static SyntaxToken GetRefKeyword => SyntaxFactory.Token(SyntaxTriviaList.Empty, SyntaxKind.RefKeyword,
+        SyntaxFactory.TriviaList(SyntaxFactory.Space));
+
     private RefTypeSyntax RewriteMethodPointerType(PointerTypeSyntax node)
     {
-        return SyntaxFactory.RefType(
-            SyntaxFactory.Token(SyntaxTriviaList.Empty, SyntaxKind.RefKeyword,
-                SyntaxFactory.TriviaList(SyntaxFactory.Space)),
-            node.ElementType.WithTriviaFrom(node));
+        return SyntaxFactory.RefType(GetRefKeyword, node.ElementType.WithTriviaFrom(node));
     }
 
     private ParameterListSyntax VisitMethodParameterList(ParameterListSyntax node)
@@ -35,27 +35,57 @@ public class UnsafeRewriter : CSharpSyntaxRewriter, ISyntaxRewriter
         return changedParameter ? node.WithParameters(parameterList) : node;
     }
 
+    private BlockSyntax FixReturnStatements(BlockSyntax node)
+    {
+        var statements = SyntaxFactory.List<StatementSyntax>();
+
+        foreach (StatementSyntax statement in node.Statements)
+        {
+            if (statement.IsKind(SyntaxKind.ReturnStatement) && statement is ReturnStatementSyntax returnStatement)
+            {
+                statements = statements.Add(returnStatement.WithExpression(SyntaxFactory.RefExpression(GetRefKeyword, returnStatement.Expression!)));
+
+                continue;
+            }
+
+            statements = statements.Add(statement);
+        }
+
+        return node.WithStatements(statements);
+    }
+
     private MemberDeclarationSyntax VisitClassMember(MemberDeclarationSyntax node)
     {
-        if (node.IsKind(SyntaxKind.MethodDeclaration))
+        if (node.IsKind(SyntaxKind.MethodDeclaration) && node is MethodDeclarationSyntax method)
         {
+            var newMethod = method;
+            bool changedReturnType = false;
+
+            if (method.ReturnType.IsKind(SyntaxKind.PointerType))
+            {
+                newMethod = newMethod.WithReturnType(RewriteMethodPointerType(newMethod.ReturnType as PointerTypeSyntax));
+                changedReturnType = true;
+            }
+
             foreach (var childNode in node.ChildNodes())
             {
-                if (childNode.IsKind(SyntaxKind.PointerType) && childNode is PointerTypeSyntax pointerType)
-                {
-                    node = node.ReplaceNode(pointerType, RewriteMethodPointerType(pointerType));
-                }
-
                 if (childNode.IsKind(SyntaxKind.ParameterList) && childNode is ParameterListSyntax parameterList)
                 {
                     var newParameterList = VisitMethodParameterList(parameterList);
 
                     if (parameterList != newParameterList)
                     {
-                        node = node.ReplaceNode(parameterList, newParameterList);
+                        newMethod = newMethod.WithParameterList(newParameterList);
                     }
                 }
+
+                if (changedReturnType && childNode.IsKind(SyntaxKind.Block) && childNode is BlockSyntax block)
+                {
+                    newMethod = newMethod.WithBody(FixReturnStatements(block));
+                }
             }
+
+            return node.ReplaceNode(method, newMethod);
         }
 
         return node;
